718aaeb1f501d61358b1df083a9f076b56820a35
Cedric Patton <ceddyp117@gmail.com>
2020-06-15 04:27:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function updateObjectWithKeyAndValue(obj, key, value){
-  newObj = Object.assign({}, obj = {prop: 1, prop2: 2});
+  var newObj = Object.assign({}, obj = {prop: 1, prop2: 2});
   return newObj;
 }
  
